313706361a589359b5f2c89d2f30b62f6a5db1ab
Juliana <juliana@le-filament.com>
2023-05-24 15:41:55 +0200

    [UPD]Update multiple choice questions

--- website_event_questions_multiple/controllers/main.py
+++ website_event_questions_multiple/controllers/main.py
@@ -2,14 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo.http import request
 
-from odoo.addons.website_event.controllers.main import WebsiteEventController
+from odoo.addons.website_event_questions.controllers.main import WebsiteEvent
 
 
-class WebsiteEvent(WebsiteEventController):
+class WebsiteEvent(WebsiteEvent):
     def _process_attendees_form(self, event, form_details):
         """Process data posted from the attendee details form.
         Extracts question answers:
-        - For both questions asked 'once_per_order' and questions asked to every attendee
         - For questions of type 'multiple_choice', extracting the suggested answer id"""
         registrations = super(WebsiteEvent, self)._process_attendees_form(
             event, form_details
@@ -20,14 +19,16 @@ class WebsiteEvent(WebsiteEventController):
 
         general_answer_ids = []
         for key, value in form_details.items():
-            if "question_answer" in key and value:
-                dummy, registration_index, question_id = key.split("-")
+            if "question_multi_answer" in key:
+                dummy, registration_index, question_answer = key.split("-")
+                question_id, answer_id = question_answer.split("_")
                 question_sudo = request.env["event.question"].browse(int(question_id))
+                answer_sudo = request.env["event.question.answer"].browse(int(answer_id))
                 answer_values = None
                 if question_sudo.question_type == "multiple_choice":
                     answer_values = {
                         "question_id": int(question_id),
-                        "value_text_box": value,
+                        "value_text_box": answer_sudo.name,
                     }
 
                 if answer_values and not int(registration_index):
