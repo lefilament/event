cb3fe1e7d863ec6a2465a092bc2152893cdab3d7
Hugo Trentesaux <hugo@le-filament.com>
2025-03-05 09:54:35 +0100

    improve readability

--- website_event_questions_multiple/controllers/main.py
+++ website_event_questions_multiple/controllers/main.py
@@ -28,38 +28,42 @@ class WebsiteEvent(WebsiteEvent):
 
         general_answer_ids = []
         for key, _value in form_details.items():
-            if "question_multi_answer" in key:
-                dummy, registration_index, question_answer = key.split("-")
-                question_id, answer_id = question_answer.split("_")
-                question_sudo = request.env["event.question"].browse(int(question_id))
+            # test html input prefix
+            if key.startswith("question_multi_answer"):
+                _html_input_prefix, registration_index_str, question_answer = key.split("-")
+                registration_index = int(registration_index_str)
+                question_id_str, answer_id = question_answer.split("_")
+                question_id = int(question_id_str)
+                question_sudo = request.env["event.question"].browse(question_id)
                 answer_sudo = request.env["event.question.answer"].browse(int(answer_id))
-                answer_values = None
-                if question_sudo.question_type == "multiple_choice":
-                    answer_values = {
-                        "question_id": int(question_id),
-                        "value_text_box": answer_sudo.name,
-                    }
-                if answer_values and not int(registration_index):
+                assert question_sudo.question_type == "multiple_choice"  # otherwise, html is malformed
+                answer_values = {
+                    "question_id": question_id,
+                    "value_text_box": answer_sudo.name,
+                }
+                # question with null registration index are general
+                if registration_index == 0:
                     general_answer_ids.append((0, 0, answer_values))
-                    mandatory_multiple_general_question_answer_count[question_id] += 1
-                elif answer_values:
-                    rindex = int(registration_index) - 1
-                    registrations[rindex][
-                        "registration_answer_ids"
-                    ].append((0, 0, answer_values))
-                    mandatory_multiple_specific_question_answer_count[rindex][question_id] += 1
+                    if question_sudo.is_mandatory_answer:
+                        mandatory_multiple_general_question_answer_count[question_id] += 1
+                # question with registration index are specific to one registration
+                else:
+                    rindex = registration_index - 1 # zero-based array ¹indexing
+                    registrations[rindex]["registration_answer_ids"].append((0, 0, answer_values))
+                    if question_sudo.is_mandatory_answer:
+                        mandatory_multiple_specific_question_answer_count[rindex][question_id] += 1
 
         # check that answers contain at least one for mandatory question
         # general
         for q, c in mandatory_multiple_general_question_answer_count.items():
             if c == 0:
-                raise UserError("Question " + q + " is mandatory but did not receive an answer.")
+                raise UserError("Question " + str(q) + " is mandatory but did not receive an answer.")
         # specific
         for r, cc in enumerate(mandatory_multiple_specific_question_answer_count):
             for q, c in cc.items():
                 if c == 0:
-                    raise UserError("Question " + q +
-                                    " is mandatory but did not receive an answer in ticket number " + r + ".")
+                    raise UserError("Question " + str(q) +
+                                    " is mandatory but did not receive an answer in ticket number " + str(r) + ".")
 
         # append general question to all items
         for registration in registrations:
